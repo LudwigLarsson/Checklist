4be42dc4a97a61bd90e27dae922a90b50bce4f29
Karaseyka <vladchuprina12@mail.ru>
2023-02-03 23:19:20 +0300

    add camera

--- app/src/main/java/ru/samsung/case2022/MainActivity.java
+++ app/src/main/java/ru/samsung/case2022/MainActivity.java
@@ -4,30 +4,42 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button bt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        bt = (Button) findViewById(R.id.scan);
+        bt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                toScanner();
+            }
+        });
     }
     public void toScanner() {
         // вызов камеры непонятно как
         // допустим, сюда передаётся фото
         // надо его кешировать и передать ссылку/id в PhotoViewerActivity
+        Intent intent = new Intent(MainActivity.this, PhotoViewerActivity.class);
+        startActivity(intent);
     }
-    public void addProduct() {
-        Intent intent = new Intent(MainActivity.this, AddProductActivity.class);
-        startActivityForResult(intent, 1);
-        public void onActivityResult(int requestCode, int resultCode, Intent data) {    //data - ?
-            switch (resultCode) {
-                case RESULT_OK:
-                    productName.setText(data.getStringExtra("et"));
-                    break;
-            }
-        }
-    }
+//    public void addProduct() {
+//        Intent intent = new Intent(MainActivity.this, AddProductActivity.class);
+//        startActivityForResult(intent, 1);
+//        public void onActivityResult(int requestCode, int resultCode, Intent data) {    //data - ?
+//            switch (resultCode) {
+//                case RESULT_OK:
+//                    productName.setText(data.getStringExtra("et"));
+//                    break;
+//            }
+//        }
+//    }
     public void editProduct() {
         Intent intent = new Intent(MainActivity.this, EditProductActivity.class);
         startActivity(intent);
--- app/src/main/java/ru/samsung/case2022/PhotoViewerActivity.java
+++ app/src/main/java/ru/samsung/case2022/PhotoViewerActivity.java
@@ -2,22 +2,69 @@ package ru.samsung.case2022;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class PhotoViewerActivity extends AppCompatActivity {
+
+    // for check
+    private static final int NORM = 1;
+    private static final int REQUEST_TAKE_PHOTO = 1;
+
+    // image
+    private ImageView iv;
+    // Button
+    private Button bt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_photo_viewer);
+        getPhoto();
+        bt = findViewById(R.id.cancel);
+        bt.setOnClickListener(v -> {
+            cancelPhoto();
+        });
+
+    }
+    // after doing photo
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        iv = (ImageView) findViewById(R.id.preview);
+        if (requestCode == NORM){Bundle extras = data.getExtras();
+            Bitmap thumbnailBitmap = (Bitmap) extras.get("data");
+            iv.setImageBitmap(thumbnailBitmap);
+
+        }
     }
     public void getPhoto() {
 
+        Intent takePhotoIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        try{
+            startActivityForResult(takePhotoIntent, REQUEST_TAKE_PHOTO);
+        }catch (ActivityNotFoundException e){
+            e.printStackTrace();
+        }
+
+
+
     }
     public void cancelPhoto() {
+        // delete photo
+        iv.setImageDrawable(null);
+        Intent intent = new Intent(PhotoViewerActivity.this, MainActivity.class);
+        startActivity(intent);
 
     }
     public void recognizePhoto() {
+        // todo Next Activity (ML)
 
     }
 }
