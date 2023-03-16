602152af3a9e5ad22d6441efab80f8ac6ad6c22a
Vitaz-Vitaz <76260006+Vitaz-Vitaz@users.noreply.github.com>
2023-03-16 18:20:15 +0300

    без количества

--- app/src/main/java/ru/samsung/case2022/ui/EditProductActivity.java
+++ app/src/main/java/ru/samsung/case2022/ui/EditProductActivity.java
@@ -21,19 +21,19 @@ public class EditProductActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_change_product);
         EditText t = findViewById(R.id.editProductName);
-        EditText t2 = findViewById(R.id.editProductCount);
+
         Button butRename = findViewById(R.id.save);
         Button butDel = findViewById(R.id.remove);
         DataBaseHandler dataBaseHandler = new DataBaseHandler(this);
         t.setText(dataBaseHandler.getProd(Util.changeI).getName());
 
-        t2.setText(String.valueOf(dataBaseHandler.getProd(Util.changeI).getCount()));
+
         butRename.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 // изменяем продукт в бд
                 DataBaseHandler dataBaseHandler = new DataBaseHandler(EditProductActivity.this);
-                dataBaseHandler.updateProd(new Products(t.getText().toString(), Integer.parseInt(t2.getText().toString())), Util.changeI);
+                dataBaseHandler.updateProd(new Products(t.getText().toString(), dataBaseHandler.getProd(Util.changeI).getCount()), Util.changeI);
                 // переходим обратно в ресайкл
                 Intent intent=new Intent(EditProductActivity.this, RootActivity.class);
                 startActivity(intent);
